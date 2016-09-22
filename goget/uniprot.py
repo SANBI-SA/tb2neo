b968be59da57d306327c50fec6b94ef14eddccf2
Thoba Lose <lose.thoba@gmail.com>
2016-09-22 15:26:50 +0200

    Preliminary UniProt Interface

--- goget/uniprot.py
+++ goget/uniprot.py
@@ -2,18 +2,27 @@ from bioservices import UniProt
 
 u = UniProt(verbose=False)
 
-columns = "id, genes, go-id, interpro, interactor, genes(PREFERRED), feature(DOMAIN EXTENT), protein names, go, " \
-          "citation, 3d, comment(FUNCTION), sequence, mass, length, families, go(biological process), " \
-          "go(molecular function), go(cellular component)"
+
+def search_uniprot(query, columns, proteome='UP000001584'):
+    query = "proteome:{}+AND+{}".format(proteome, query)
+
+    result = u.search(query=query, frmt="tab", columns=columns, sort=None)
+    return result
 
 
-def query_uniprot(locus_tags, proteome='UP000001584'):
+def query_uniprot(locus_tags):
+    """
+    Get data from UniProt
+    :param locus_tags:
+    :return:
+    """
+    columns = "id, genes, go-id, interpro, interactor, genes(PREFERRED), feature(DOMAIN EXTENT), protein names, go, " \
+              "citation, 3d, comment(FUNCTION), sequence, mass, length, families, go(biological process), " \
+              "go(molecular function), go(cellular component)"
     result = None
     for tag_list in locus_tags:
-        gene_query = '(' + '+OR+'.join(['gene:' + name for name in tag_list]) + ')'
-        query = "proteome:{}+AND+{}".format(proteome, gene_query)
-
-        result = u.search(query, frmt="tab", columns=columns, sort=None)
+        query = '(' + '+OR+'.join(['gene:' + name for name in tag_list]) + ')'
+        result = search_uniprot(query, columns)
         print(result)
 
     return result
