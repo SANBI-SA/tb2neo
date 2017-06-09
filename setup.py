2c338084b3842579649601c953bd94947f3e688e
Peter van Heusden <pvh@sanbi.ac.za>
2017-06-09 19:35:41 +0200

    Fixed setup.py: description and packages

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='tb2neo',
-    version='0.0.4',
-    description='Parses M. tuberculosis annotation (in GFF file and online sources) and builds a Neo4j graph database',
+    version='0.0.5',
+    description='Parses M. tuberculosis annotation (in GFF file and online sources) and builds a Neo4j graph database'\
                 'storing the annotate features. It also maps these features to external services such as UniProt.',
     keywords='neo4j, bioservices, gff',
-    py_modules=['tb2neo'],
+    packages=['tb2neo'],
     install_requires=[
         'click',
         'bioservices',
